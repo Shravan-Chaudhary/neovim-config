9329c5312a3f308ad4e79b1401c3fbdc452b8077
Shravan-Chaudhary <shravansinghchaudhary@gmail.com>
2024-04-06 04:09:52 +0530

    update keymaps

--- lua/shravan/core/keymaps.lua
+++ lua/shravan/core/keymaps.lua
@@ -2,6 +2,8 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
+local opts = { noremap = true, silent = true }
+
 keymap.set("n", "<leader>d", function() vim.cmd('bd') end)
 vim.api.nvim_set_keymap('t', '<Esc>', [[<C-\><C-n>]], { noremap = true })
 
@@ -35,6 +37,28 @@ vim.o.completeopt = "menuone,noinsert,noselect"
 
 vim.keymap.set('n', '<C-d>', '<C-d>zz', { noremap = true })
 vim.keymap.set('n', '<C-u>', '<C-u>zz', { noremap = true })
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- New tab
+keymap.set("n", "te", ":tabedit")
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+-- Split window
+keymap.set("n", "ss", ":split<Return>", opts)
+keymap.set("n", "sv", ":vsplit<Return>", opts)
+-- Move window
+keymap.set("n", "sh", "<C-w>h")
+keymap.set("n", "sk", "<C-w>k")
+keymap.set("n", "sj", "<C-w>j")
+keymap.set("n", "sl", "<C-w>l")
+
+-- Resize window
+keymap.set("n", "<C-w><left>", "<C-w><")
+keymap.set("n", "<C-w><right>", "<C-w>>")
+keymap.set("n", "<C-w><up>", "<C-w>+")
+keymap.set("n", "<C-w><down>", "<C-w>-")
 
 vim.api.nvim_set_keymap('n', '<leader>ga', ':Git add %<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>gc', ':Git commit<CR>', { noremap = true })
